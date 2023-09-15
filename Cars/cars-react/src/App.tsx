40a134ee45da5d8985eb6703d17d251723408d9e
danielcasanova12 <119464431+danielcasanova12@users.noreply.github.com>
2023-09-15 11:39:44 -0300

    React

--- Cars/cars-react/src/App.tsx
+++ Cars/cars-react/src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
 
 interface Car {
   model: string;
@@ -29,41 +30,32 @@ const AddCar: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-  
+
     try {
-      const response = await fetch('https://localhost:7016/api/cars', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(carData),
-      });
-  
-      if (response.ok) {
+      const response = await axios.post('https://localhost:7016/api/cars', carData);
+
+      if (response.status === 201) {
         console.log('Carro adicionado com sucesso!');
         // Atualize a lista de carros após a adição bem-sucedida, se desejar.
       } else {
-        const errorMessage = `Erro ao adicionar o carro: ${response.status} - ${response.statusText}`;
-        console.error(errorMessage);
+        console.error(`Erro ao adicionar o carro: ${response.status} - ${response.statusText}`);
       }
     } catch (error) {
       console.error('Erro ao enviar a solicitação:', error);
     }
   };
-  
 
   // Função para buscar a lista de carros da API
   const fetchCarList = async () => {
     try {
-      const response = await fetch('https://localhost:7016/api/cars');
-      if (response.ok) {
-        const data = await response.json();
-        setCarList(data); // Atualiza o estado com os dados da lista de carros
+      const response = await axios.get('https://localhost:7016/api/cars');
+      if (response.status === 200) {
+        setCarList(response.data); // Atualiza o estado com os dados da lista de carros
       } else {
         console.error('Falha ao buscar a lista de carros.');
       }
     } catch (error) {
-      console.error('Erro ao enviar a solicitação:');
+      console.error('Erro ao enviar a solicitação:', error);
     }
   };
 
@@ -76,7 +68,34 @@ const AddCar: React.FC = () => {
     <div>
       <h2>Adicionar Carro</h2>
       <form onSubmit={handleSubmit}>
-        {/* ... Seus campos de entrada para adicionar carros aqui */}
+        <input
+          type="text"
+          name="model"
+          placeholder="Modelo"
+          value={carData.model}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="color"
+          placeholder="Cor"
+          value={carData.color}
+          onChange={handleInputChange}
+        />
+        <input
+          type="number"
+          name="year"
+          placeholder="Ano"
+          value={carData.year}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="photoUrl"
+          placeholder="URL da foto"
+          value={carData.photoUrl}
+          onChange={handleInputChange}
+        />
         <button type="submit">Adicionar Carro</button>
       </form>
 
